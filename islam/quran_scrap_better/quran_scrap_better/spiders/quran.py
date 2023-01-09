ab6bbf4d9c5ca5ec374d99f75253073db3075327
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-09 08:18:40 +0700

    just an example 2

--- islam/quran_scrap_better/quran_scrap_better/spiders/quran.py
+++ islam/quran_scrap_better/quran_scrap_better/spiders/quran.py
@@ -6,16 +6,15 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://quotes.toscrape.com/page/1/',
-            'https://quotes.toscrape.com/page/2/',
+            "https://quran.toscrape.com/page/1/",
+            "https://quran.toscrape.com/page/2/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
+        filename = f"quotes-{page}.html"
+        with open(filename, "wb") as f:
             f.write(response.body)
-        self.log(f'Saved file {filename}')
-        
+        self.log(f"Saved file {filename}")
