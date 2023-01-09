ff911decdb5553a2fd3555f70b1597a7cc053d55
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-09 08:22:39 +0700

    let's try

--- islam/quran_scrap_better/quran_scrap_better/spiders/quotes.py
+++ islam/quran_scrap_better/quran_scrap_better/spiders/quotes.py
@@ -6,8 +6,8 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://quran.toscrape.com/page/1/",
-            "https://quran.toscrape.com/page/2/",
+            "https://quotes.toscrape.com/page/1/",
+            "https://quotes.toscrape.com/page/2/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
