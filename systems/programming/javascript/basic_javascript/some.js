ae8afc4a9134a886bc13f1f4b0d2be1ea31d619d
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-24 17:33:35 +0700

    Let's learn `basic_javascript`

---
+++ systems/programming/javascript/basic_javascript/some.js
@@ -0,0 +1,25 @@
+//Source: https://www.youtube.com/watch?v=PkZNo7MFNFg
+//0:00:00 Introduction
+//0:01:24 Running JavaScript
+//0:04:23 Comment Your Code
+//0:05:56 Declare Variables
+//0:06:15 Storing Values with the Assignment Operator
+//0:11:31 Initializing Variables with the Assignment Operator
+//0:11:58 Uninitialized Variables
+//0:12:40 Case Sensitivity in Variables
+//0:14:05 Basic Math
+//0:15:30 Increment and Decrement
+//0:16:22 Decimal Numbers
+//0:16:48 Multiply Two Decimals
+//0:17:18 Divide Decimals
+//0:17:33 Finding a Remainder
+//0:18:22 Augmented Math Operations
+//0:21:19 Declare String Variables
+//0:22:01 Escaping Literal Quotes
+//0:23:44 Quoting Strings with Single Quotes
+//0:25:18 Escape Sequences
+//0:26:46 Plus Operator
+//0:27:49 Plus Equals Operator
+//0:29:01 Constructing Strings with Variables
+//0:30:14 Appending Variables to Strings
+//0:31:11 Length of a String
