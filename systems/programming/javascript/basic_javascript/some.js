988c900cb0e8b9d1cd5995afe4561d14fcbc2fea
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-25 12:44:16 +0700

    Constructing Strings with Variables

--- systems/programming/javascript/basic_javascript/some.js
+++ systems/programming/javascript/basic_javascript/some.js
@@ -226,5 +226,11 @@ someString += "G."
 console.log(someString)
 console.log(' ')
 //0:29:01 Constructing Strings with Variables
+console.log('Constructing Strings with Variables')
+console.log('___________________________________')
+var im = "3"
+var what = "IM" + im + "asa jadi lebih mahal. engga beb. oh iya atuh :*"
+console.log(what)
+console.log(' ')
 //0:30:14 Appending Variables to Strings
 //0:31:11 Length of a String
