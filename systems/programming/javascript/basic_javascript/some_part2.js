08d1bcb1dc8dc80168a687d819ba040ada1d45c8
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-25 17:47:33 +0700

    Let's learn `basic_javascript` part 2

---
+++ systems/programming/javascript/basic_javascript/some_part2.js
@@ -0,0 +1,13 @@
+//0:32:01 Bracket Notation
+//0:33:27 Understand String Immutability
+//0:34:23 Find the Nth Character
+//0:36:28 Word Blanks
+//0:40:44 Arrays
+//0:41:43 Nest Arrays
+//0:42:33 Access Array Data
+//0:43:34 Modify Array Data
+//0:44:48 Access Multi-Dimensional Arrays
+//0:46:30 push()
+//0:47:29 pop()
+//0:48:33 shift()
+//0:49:23 unshift()
