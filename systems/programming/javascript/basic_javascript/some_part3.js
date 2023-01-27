0ef4c2819dedf03fdf1fd4c41d1a26df32dc51de
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-27 09:20:10 +0700

    Write Reusable with Functions

--- systems/programming/javascript/basic_javascript/some_part3.js
+++ systems/programming/javascript/basic_javascript/some_part3.js
@@ -5,20 +5,10 @@ var shoppingList = [[eggs, 2], [cabbage, 4], [tomato, 3]]
 console.log(shoppingList)
 console.log(' ')
 //0:51:41 Write Reusable with Functions
-//0:53:41 Arguments
-//0:55:43 Global Scope
-//0:59:31 Local Scope
-//1:00:46 Global vs Local Scope in Functions
-//1:02:40 Return a Value from a Function
-//1:03:55 Undefined Value returned
-//1:04:52 Assignment with a Returned Value
-//1:05:52 Stand in Line
-//1:08:41 Boolean Values
-//1:09:24 If Statements
-//1:11:51 Equality Operators
-//1:19:17 And / Or Operators
-//1:21:37 Else Statements
-//1:22:27 Else If Statements
-//1:23:30 Logical Order in If Else Statements
-//1:24:45 Chaining If Else Statements
-//1:27:45 Golf Code
+console.log('Write Reusable with Functions')
+console.log('_____________________________')
+function aReusableFunction() {
+    console.log("Hello world.")
+}
+aReusableFunction()
+console.log(' ')
