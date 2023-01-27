86db39295cc7c0b440f9f0b4d206feea831c3001
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-27 14:07:13 +0700

    Let's learn `basic_javascript` part 4

---
+++ systems/programming/javascript/basic_javascript/some_part4.js
@@ -0,0 +1,44 @@
+//1:32:15 Switch Statements
+//1:41:11 Returning Boolean Values from Functions
+//1:42:20 Return Early Pattern for Functions
+//1:43:38 Counting Cards
+//1:49:11 Build Objects
+//1:50:46 Dot Notation
+//1:51:33 Bracket Notation
+//1:52:47 Variables
+//1:53:34 Updating Object Properties
+//1:54:30 Add New Properties to Object
+//1:55:19 Delete Properties from Object
+//1:55:54 Objects for Lookups
+//1:57:43 Testing Objects for Properties
+//1:59:15 Manipulating Complex Objects
+//2:01:00 Nested Objects
+//2:01:53 Nested Arrays
+//2:03:06 Record Collection
+//2:10:15 While Loops
+//2:11:35 For Loops
+//2:13:56 Odd Numbers With a For Loop
+//2:15:28 Count Backwards With a For Loop
+//2:17:08 Iterate Through an Array with a For Loop
+//2:19:43 Nesting For Loops
+//2:22:45 Do...While Loops
+//2:24:12 Profile Lookup
+//2:28:18 Random Fractions and Whole Numbers
+//2:31:46 parseInt Function
+//2:33:29 Ternary Operator
+//2:34:57 Multiple Ternary Operators
+//2:36:57 var vs let
+//2:41:32 const Keyword
+//2:43:40 Mutate an Array Declared with const
+//2:44:52 Prevent Object Mutation
+//2:47:17 Arrow Functions
+//2:53:04 Default Parameters
+//2:54:00 Rest Operator
+//2:55:31 Spread Operator
+//2:57:18 Destructuring Assignment
+//3:06:39 Template Literals
+//3:10:43 Simple Fields
+//3:12:24 Declarative Functions
+//3:12:56 class Syntax
+//3:15:11 getters and setters
+//3:20:25 import and export
