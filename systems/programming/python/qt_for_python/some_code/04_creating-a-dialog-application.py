88c81eff8e2fcb7e722a55ff8c5e1755d5de0221
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-02-13 17:24:25 +0700

    Created `04_creating-a-dialog-application.py`

---
+++ systems/programming/python/qt_for_python/some_code/04_creating-a-dialog-application.py
@@ -0,0 +1,19 @@
+# Creating a Dialog Application
+import sys
+from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("My Form")
+
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
