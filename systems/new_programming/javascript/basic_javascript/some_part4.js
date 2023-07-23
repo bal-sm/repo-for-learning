1ae07813419c191eeb9ef47fac1930202518e338
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 17:01:24 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -694,6 +694,7 @@ I am ${person.age} years old.`; // Ini teh gening gaganti ` dari " tapi mau ubah
 // terus sama bisa nambahin pake new line beneran, sama " atau ' beneran
 // ini teh guys, aku terpaksa guys, gabisa anjing tanpa js teh, that's hard truth for web devs guys.
 // yang penting HRD perusahaan mau guys
+// ECMA ECMA guys, kata aing mah buang, ganti ke Python gak sih guys
 console.log(greeting); 
 
 
