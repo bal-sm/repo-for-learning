a599c9ab7cbe9585b5d6fd14f14cf250d59d5b3f
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 16:43:08 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -654,6 +654,24 @@ function removeFirstTwo(list) {
 const arr = removeFirstTwo(source);
 console.log(arr); 
 console.log(source);
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+
+    return function half(stats) {
+        return (stats.max + stats.min) / 2.0;
+    };
+
+})();
+console.log(stats); 
+console.log(half(stats)); 
 console.log("3:06:39 Template Literals")
 console.log("____________________")
 console.log("3:10:43 Simple Fields")
