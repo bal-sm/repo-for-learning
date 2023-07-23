3e17c768d469bca2a97618a90e4337eac2473619
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 17:00:25 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -693,6 +693,7 @@ const greeting = `Hello, my name is ${person.name}!
 I am ${person.age} years old.`; // Ini teh gening gaganti ` dari " tapi mau ubah ubah gitu pake $, biar kayak Python, gimana aing we, males lookup da ngapain
 // terus sama bisa nambahin pake new line beneran
 // ini teh guys, aku terpaksa guys, gabisa anjing tanpa js teh, that's hard truth for web devs guys.
+// yang penting HRD perusahaan mau guys
 console.log(greeting); 
 
 
