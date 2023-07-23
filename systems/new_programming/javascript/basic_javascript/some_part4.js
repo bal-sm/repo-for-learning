bb718b3fd476039ead7a65e912416e124e7fba2a
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 11:52:57 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -521,7 +521,7 @@ console.log("____________________")
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
 const squareList = (arr) => {
-    const squaredIntegers = arr;
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
     return squaredIntegers;
 };
 
