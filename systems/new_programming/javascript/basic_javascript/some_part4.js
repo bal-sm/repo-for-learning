9cf6dd59abe8de16885e371c16d49f417564c1d7
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 16:21:41 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -622,8 +622,8 @@ console.log("____________________")
 // 
 // console.log("getMaxOfTmrw(LOCAL_FORECAST) = " + getMaxOfTmrw(LOCAL_FORECAST));
 // Use Destructuring Assignment to Assign Variables from Arrays
-const [z, x, , y] = [1, 2, 3, 4, 5, 6]; // EW, WTF
-console.log(z, x, y);
+const [zBUibc, xNSAJNDO, , ydonfowenf] = [1, 2, 3, 4, 5, 6]; // EW, WTF
+console.log(zBUibc, xNSAJNDO, ydonfowenf);
 
 let aZzxzzxzx = 8, bZxxzzx = 6;
 
