7afb42d3f1816c35cd1232d77f1265dc330159b6
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-23 13:47:16 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part4.js
+++ systems/new_programming/javascript/basic_javascript/some_part4.js
@@ -561,7 +561,7 @@ const sumRestZxzxz = (function() {
     };
 })();
 // tuh jadinya ...args
-console.log("sumRestZxzxz(1, 2, 3, 4) = " + sumRestZxzxz(1, 2, 3))
+console.log("sumRestZxzxz(1, 2, 3, 4) = " + sumRestZxzxz(1, 2, 3, 4))
 console.log("2:55:31 Spread Operator")
 console.log("____________________")
 console.log("2:57:18 Destructuring Assignment")
