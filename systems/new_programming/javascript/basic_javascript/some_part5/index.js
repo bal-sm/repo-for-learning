c8231920dc66e51c1480ca90b2a3d3423832da51
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-24 22:42:49 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part5/index.js
+++ systems/new_programming/javascript/basic_javascript/some_part5/index.js
@@ -3,3 +3,7 @@ import { capitalizeString } from "./string_function";
 const cap = capitalizeString("hello!");
 
 console.log(cap);
+
+// Sumpah IH gak bisa soalnya lewat browser gitu dan lewat file:// protocol
+// BUT whatever the frick, 'cause JavaScript kok bangga
+// lagipula banyak dipake buat yang pake node.js
