9c9c97bf816127b57550c0fc0c1edce201fe9776
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-25 00:44:21 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part5/index.js
+++ systems/new_programming/javascript/basic_javascript/some_part5/index.js
@@ -7,3 +7,5 @@ console.log(cap);
 // Sumpah IH gak bisa soalnya lewat browser gitu dan lewat file:// protocol
 // BUT whatever the frick, 'cause JavaScript kok bangga
 // lagipula banyak dipake buat yang pake node.js
+
+// FIX THIS PLZ, LEWAT NODE
