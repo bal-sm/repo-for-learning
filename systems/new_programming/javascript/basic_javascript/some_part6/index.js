32d897820ac5428d789257b96bb5257ef2110787
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-25 13:48:03 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part6/index.js
+++ systems/new_programming/javascript/basic_javascript/some_part6/index.js
@@ -19,3 +19,4 @@ import * as capitalizeStrings from "capitalize_strings";
 // Create an Export Fallback with export default
 
 export default function subtract(x,y) {return x - y;} // let's turn it to a fallback export, DONE
+// ----------^ that is a grate fallback export
