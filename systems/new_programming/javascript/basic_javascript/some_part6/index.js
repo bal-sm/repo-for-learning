6f6f96ab9bdf30707f07126e160c1afcddd1cf5c
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-25 13:45:05 +0700

    Continued `programming` `javascript` `basic_javascript`

--- systems/new_programming/javascript/basic_javascript/some_part6/index.js
+++ systems/new_programming/javascript/basic_javascript/some_part6/index.js
@@ -9,3 +9,8 @@ export const bar = "foo";
 
 // udah we disini dulu aja lah
 // soalnya gak work as expected, harus di node.js
+
+// Use * to Import Everything from a File
+
+import * as capitalizeStrings from "capitalize_strings";
+// "./capitalize_strings" if it's on the same directory
