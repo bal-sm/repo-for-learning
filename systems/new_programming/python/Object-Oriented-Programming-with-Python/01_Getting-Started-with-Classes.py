9f3c0a79e6a4774a1e589a5a81cd7608b5c61842
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-07-31 11:30:08 +0700

    Created `01_Getting-Started-with-Classes.py` on `Object-Oriented-Programming-with-Python`

---
+++ systems/new_programming/python/Object-Oriented-Programming-with-Python/01_Getting-Started-with-Classes.py
@@ -0,0 +1,17 @@
+# Getting Started with Classes
+
+item1 = "Phone"
+item1_price = 100
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quantity))
+print(type(item1_price_total))
+
+# The output:
+# <class 'str'>
+# <class 'int'>
+# <class 'int'>
+# <class 'int'>
