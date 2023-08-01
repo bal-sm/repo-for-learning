f0c4dfd020d848449b72989aef5b858f8e394b87
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-08-01 13:21:18 +0700

    Continued `python_django` `django+htmx`

--- systems/new_programming/python_django/django+htmx/films/views.py
+++ systems/new_programming/python_django/django+htmx/films/views.py
@@ -7,18 +7,25 @@ from django.contrib.auth import get_user_model
 
 from films.forms import RegisterForm
 
+
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'index.html'
-    
+    template_name = "index.html"
+
+
 class Login(LoginView):
-    template_name = 'registration/login.html'
+    template_name = "registration/login.html"
+
 
 class RegisterView(FormView):
     form_class = RegisterForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("login")
 
     def form_valid(self, form):
         form.save()  # save the user
         return super().form_valid(form)
+
+
+def check_username(request):
+    pass
