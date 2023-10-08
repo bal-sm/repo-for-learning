90f5d836dcaa4f18ce45be339284880eb4e88209
Mahmuda <bal.mahmuda@gmail.com>
2023-10-08 18:03:23 +0700

    Used `pprint`

--- systems/programming/python/Object-Oriented-Programming-with-Python/02_Item_class/phone.py
+++ systems/programming/python/Object-Oriented-Programming-with-Python/02_Item_class/phone.py
@@ -31,6 +31,6 @@ phone_1.broken_phones = 2
 phone_2 = Phone(name="iPhone 15 Pro max", price=30_000_000, quantity=5)
 phone_2.broken_phones = 4
 
-print(Phone.all)
+pprint(Phone.all)
 
 # 'cause overheating issues.
