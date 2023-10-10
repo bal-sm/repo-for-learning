f13bed68a8d2f2e41933327624ea402c4a71598c
Mahmuda <bal.mahmuda@gmail.com>
2023-10-10 12:32:25 +0700

    Created `both_entries.py`

---
+++ systems/programming/python/OOP-with-Python/02_Item_class/both_entries.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+from main import Item
+from phone import Phone
+
+
+phone_wow = Phone(name="iPhone 15 Pro max", price=30_000_000, quantity=5)
+phone_wow.broken_phones = 4
+
+motorcycle = Item(name="Vario", price=10_000_000, quantity=2_000)
+
+print("Item.all =")
+pprint(Item.all)
+
+print("Phone.all =")
+pprint(Phone.all)
+
+# frick Item.all = Phone.all
