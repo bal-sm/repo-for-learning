0b62552ad31088740e968e2f55f235129da7a21c
Mahmuda <bal.mahmuda@gmail.com>
2023-10-16 12:41:53 +0700

    Added a test to check if there will be an exception(sic)

--- systems/programming/python/OOP-with-Python/02_Item_class/2_encapsulation_testing.py
+++ systems/programming/python/OOP-with-Python/02_Item_class/2_encapsulation_testing.py
@@ -3,3 +3,8 @@ from classes.main import Item
 item1 = Item(name="My Item", price=750)
 
 print(item1.read_only_name)
+
+try:
+    item1.read_only_name = "Something else"
+except:
+    print("Tuh error cenah")
